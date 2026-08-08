05bc0f9e4b6b009a6dae6d0ee1ba40cd8ddd209b
Sagar Jamadar <sagarjamadar8989@gmail.com>
2026-08-08 18:20:47 +0530

    Navbar Created

---
+++ frontend/src/layout/Navbar.tsx
@@ -0,0 +1,37 @@
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { IoStorefrontOutline } from 'react-icons/io5';
+import { LuLayoutDashboard } from "react-icons/lu";
+import { MdKeyboardArrowDown } from "react-icons/md";
+function Navbar() {
+  return (
+    <nav className='flex justify-between bg-white p-5 border-solid border-b-2 border-gray-300' >
+
+      <div className="flex items-center gap-5">
+        <LuLayoutDashboard className='h-6 w-6' />
+        <div>
+          <h2 className="text-xl font-medium">DashBaoard</h2>
+          <span className='text-gray-400'> Here's what's happening in your shop today </span>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <button className=' flex items-center gap-2 border-solid border-2 border-gray-300 p-3 rounded-3xl font-medium'>
+          <IoStorefrontOutline className='h-5 w-5'/>
+          Grocery & and General
+          <MdKeyboardArrowDown className='h-6 w-6'/>
+        </button>
+        <IoIosNotificationsOutline className='h-8 w-8' />
+        <div className="flex gap-2">
+          <div className=" bg-blue-100 rounded-full p-3 text-blue-500 ">RK</div>
+          <div>
+            <h4 className='text-lg font-medium'>Rahul Kumar</h4>
+            <div className='text-sm'>Owner</div>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import Navbar from '../layout/Navbar'
 
 function Dashboard() {
   return (
     <div>
+      <Navbar/>
       Dashboard
     </div>
   )
